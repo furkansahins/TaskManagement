cb32387fde90cdcc194b897e2bd01308f662c952
furkansahins <furkansahin1902@gmail.com>
2026-01-02 20:47:23 +0300

    Database Sınıfı eklendi

---
+++ src/com/example/taskmanager/Database/DatabaseConnection.java
@@ -0,0 +1,25 @@
+package com.example.taskmanager.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/task_manager";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "445801";
+
+    private static Connection connection;
+
+    private DatabaseConnection() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+}
